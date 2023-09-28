078f3d1d108aa5c0e597fec71a1baefa30b6be5c
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-09-29 04:03:54 +0530

    everything responsive till now

--- src/components/Layout/Header/HeaderCartButton.tsx
+++ src/components/Layout/Header/HeaderCartButton.tsx
@@ -4,7 +4,7 @@ import Button from "../../UI/Button";
 
 const HeaderCartButton = () => {
   return (
-    <Button className="h-12 w-36">
+    <Button className="mr-[6%] h-12 w-36 sm:mr-[8%]">
       <span className="mx-1 inline-block h-[14px]">{FaCartShopping()}</span>
       <span className="mx-1 inline-block">Food Cart</span>
       <span className="mx-1 inline-block">3</span>
--- src/components/Layout/HomeImageContainer.tsx
+++ src/components/Layout/HomeImageContainer.tsx
@@ -7,16 +7,16 @@ import foodImage from "../../assets/food.png";
 
 const HomeImageContainer = () => {
   return (
-    <Container className="z-0 h-[25rem] w-[100%] overflow-hidden bg-[#f9e5ca]">
+    <Container className="z-0 ml-[-2rem] h-[25rem] w-[115%] overflow-hidden bg-[#f9e5ca] sm:h-[30rem] sm:w-[110%] lg:h-[40rem] lg:p-5">
       <Image
         src={headerImage}
         alt="Khao Piyo Aish Karo"
-        className="cover h-[100%] w-[110%] translate-x-4 translate-y-4 rotate-6"
+        className="cover h-[100%] w-[110%] translate-x-4 translate-y-4 rotate-6  overflow-hidden sm:h-[30rem] sm:w-[110%] lg:h-[40rem]"
       />
       <Image
         src={foodImage}
         alt="Tasty Punjabi Food Icon"
-        className="absolute top-[12.75rem] w-[50%]"
+        className="absolute left-5 top-[14rem] h-40 w-[50%] sm:left-[8%] sm:top-64 sm:h-[14rem] sm:w-[17.5rem] lg:top-[22rem] lg:h-[18rem] lg:w-[32%] xl:top-[20rem] xl:h-[20rem] xl:w-[28%]"
       />
     </Container>
   );
